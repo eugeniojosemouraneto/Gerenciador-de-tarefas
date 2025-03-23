c0390aaef57d9aae4f9179e24825e5f8e07c2ad2
Eugenio <eugeniojosemouraneto092@gmail.com>
2025-03-23 19:05:59 -0300

    feat(backend/core): inclusão de todos os endpoints do app accounts na aplicação

--- backend/accounts/views/view_signin.py
+++ backend/accounts/views/view_signin.py
@@ -12,10 +12,11 @@ class signin ( APIView ):
     def post ( self, request ):
         email = request.data.get( 'email' )
         password = request.data.get( 'password' )
-        user_instance: user = authentication.sigin( email = email, password = password ) 
+        auth = authentication()
+        user_instance: user = auth.sigin( email = email, password = password ) 
         token = RefreshToken.for_user( user_instance )
         return Response({
             "user"        : user_serializers( user_instance ).data,
-            "refresh"     : str(token.refresh),
+            "refresh"     : str(token),
             "acess_token" : str(token.access_token)
         })
--- backend/accounts/views/view_signup.py
+++ backend/accounts/views/view_signup.py
@@ -1,8 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from rest_framework_simplejwt.tokens import RefreshToken
-
 from accounts.auth import authentication
 from accounts.models import user
 from accounts.serializers import user_serializers
@@ -13,7 +11,8 @@ class signup ( APIView ):
         name = request.data.get( 'name' )
         email = request.data.get( 'email' )
         password = request.data.get( 'password' )
-        user_instance: user = authentication.sigup( name = name, email = email, password = password )
+        auth = authentication()
+        user_instance: user = auth.sigup( name = name, email = email, password = password )
         return Response({
             "user"        : user_serializers( user_instance ).data,
         })
